8c79d8ef09177f17c6faf619c64f8e061d8378a4
zhang <zhang@example.com>
2017-12-14 23:55:53 +0800

    第一次上交12/14

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="dell" @click="handleClick">{{message}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        message: 'hello world'
+      }
+    },
+    methods: {
+      handleClick () {
+        this.message = 'bye world'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .dell {
+    color: red;
+  }
+</style>
